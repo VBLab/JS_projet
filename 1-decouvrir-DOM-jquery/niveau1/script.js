f205921e9771434d63258ec421e291c93a1d86ed
VBLab <58396766+VBLab@users.noreply.github.com>
2020-03-20 11:26:08 +0100

    niveau1

--- 1-decouvrir-DOM-jquery/niveau1/script.js
+++ 1-decouvrir-DOM-jquery/niveau1/script.js
@@ -1,39 +1,12 @@
-
-// il faut d'abord avec la bonne fonction....inséer la liste des films...= ouvrir le fichier playlist.txt
-
 $(document).ready(function(){
-        $("#ui").children("button").click(function(){
-            $.get('./playlist.txt', function(data){
-                spiltFile(data);
-                htmlDivElement();
-
-                console.log(data.split('\n'));
-            });
-       });
-    });
-
-
-    
-    //$.each(donnée, function(index, value ){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $("#ui").children("button").click(function(){
+        $("#list").empty();
+        $.get('./playlist.txt', function(data){
+         var monString = (data.split('\n'));
+         $.each(monString, function(index,value){
+             $("#list").append("<li>"+value+"</li>");
+         })   
+           // console.log(data.split('\n'));
+        });
+   });
+});
